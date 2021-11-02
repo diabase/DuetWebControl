129bd6b09517d962d032dd8bfe6a36384fdaf528
Ronald.Thomas <Ronald.Thomas@gmail.com>
2021-11-02 16:24:51 -0600

    Use the Fans Panel from the Status Tab on the Dashboard Tab

--- src/routes/Control/Dashboard.vue
+++ src/routes/Control/Dashboard.vue
@@ -17,10 +17,6 @@
 			</v-row>
 
 			<v-row class="my-3">
-				<v-col sm="12" :md="(!isFFForUnset && atxPower !== null) ? 9 : 12" :lg="(!isFFForUnset && atxPower !== null) ? 9 : 12" :xl="(!isFFForUnset && atxPower !== null) ? 10 : 12">
-					<fan-panel></fan-panel>
-				</v-col>
-
 				<v-col v-if="!isFFForUnset && atxPower !== null" md="3" lg="3" xl="2" align-self="center">
 					<atx-panel></atx-panel>
 					<turret-lock-panel></turret-lock-panel>
@@ -29,7 +25,12 @@
 		</v-col>
 
 		<v-col class="hidden-xs-only" sm="4" md="4" lg="3" xl="3">
-			<macro-list></macro-list>
+			<v-col cols="12">
+				<macro-list></macro-list>
+			</v-col>
+			<v-col cols="12">
+				<fans-panel></fans-panel>
+			</v-col>
 		</v-col>
 	</v-row>
 </template>
